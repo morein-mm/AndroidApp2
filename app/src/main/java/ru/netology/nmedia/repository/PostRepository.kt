ab56c2eb66a7293af36547e46edfe0b34ae2b73a
morein <morein.mm@gmail.com>
2024-05-04 16:46:45 +0300

    OkHttp enqueue

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -5,10 +5,9 @@ import java.lang.Exception
 
 interface PostRepository {
     fun getAll(callback: Callback<List<Post>>)
-    fun likeById(id: Long): Post
-    fun save(post: Post)
-    fun removeById(id: Long)
-    fun getById(id: Long): Post
+    fun likeById(id: Long, likedByMe: Boolean, callback: Callback<Post>)
+    fun save(post: Post, callback: Callback<Post>)
+    fun removeById(id: Long, callback: Callback<Boolean>)
 
     interface Callback<T> {
         fun onSuccess(result: T)
